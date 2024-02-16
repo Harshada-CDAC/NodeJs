bdfc7ede25e8dca2ca0c9243be03636c80a587a9
Harshada Patil <142893428+Harshada-CDAC@users.noreply.github.com>
2024-02-16 11:31:38 +0530

    Delete Node Js Practice 1 directory

--- Node Js Practice 1/index.js
+++
@@ -1,9 +0,0 @@
-
-const http=require('http');
-const data =require('./data');
-const { json } = require('stream/consumers');
-http.createServer((req,resp)=>{
-    resp.writeHead(500,{'content-Type':'application\json'});
-    resp.write(JSON.stringify(data));
-    resp.end();
-}).listen(5000);
